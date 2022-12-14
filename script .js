70f607b31f83e40e50ed519be873637f95646eeb
Fadekemi001 <faizatfadekemi@gmail.com>
2022-12-14 01:40:59 +0100

    add existing file

---
+++ script .js
@@ -0,0 +1,211 @@
+// console.log("Hello World")
+// alert("Hello Guys")
+
+// Declaring a variable(var, let const)
+
+// var firstName = "Ricky"
+// var firstName = "Daniel"
+// let age = 34
+
+//const firstName = "Ricky"
+
+// Data types --> (string, number, undefined, null, boolean, object, array)
+// const firstName = "Ricky" // string
+// let age = 45 // number
+// let value // undefined
+// const x = null; 
+// const isPerson = true; // Boolean
+
+
+// Array
+// let numbers = [3, 7, 9, 2, 1]
+// let fruits = ["apple", "mango", "pears"]
+
+
+// object
+// const laptop = {
+//     name : "Hp",
+//     version : "Core i7",
+//     isPerson : true,
+//     hobbies : ["Video games", "sleeping", "eating"]
+    
+// }
+
+// String Concatenation
+// let name = "Basit"
+// let age = 33
+
+// // let result = 'My name is ' + name + ' and I am ' + age
+// console.log('My name is ' + name + ' and I am ' + 3+8)
+// // Template literal
+// console.log(`My name is ${name} and I am ${4* 4}`)
+
+// String methods and properties
+// let one = "Hello World"
+// let sapa;
+
+// Get length
+// sapa = one.length
+
+// getting the index
+// sapa = one[4]
+
+//Change case
+//sapa = one.toUpperCase();
+// sapa = one.toLowerCase()
+
+// Get the substring
+// sapa = one.substring(0, 4)
+
+// Array methods and properties
+const colors = ['black', 'yellow', 'blue', 'white'];
+ let two;
+ colors.shift();
+ console.log(colors)
+
+// Getting lenght
+// sapa = colors.length
+
+// index
+// sapa = colors[3]
+
+// adding to the end of an array
+// colors.push('grey')
+
+// remove at the end
+// colors.pop()
+
+// add at the beginning
+// colors.unshift('purpule')
+
+// remove at the beginning
+//colors.shift()
+
+
+// Arithmetic Operators --> +, -, /, *, %, **, ++, --
+
+// let a = 4
+// let b = 2
+
+// Object Literial
+
+// const laptop = {
+//     name : "Hp",
+//     version : "Core i7",
+//     isPerson : true,
+//     hobbies : ["Video games", "sleeping", "eating"]
+    
+// }
+
+// laptop.email = 'laptop@gmail.com'
+// laptop.hobbies.push('singing')
+
+// const users = [
+//     {
+//         name : 'person1',
+//         age : 24,
+//         email : "person1@gmail.com"
+//     },
+//     {
+//         name : 'person2',
+//         age : 26,
+//         email : "person2@gmail.com"
+//     },
+//     {
+//         name : 'person3',
+//         age : 28,
+//         email : "person3@gmail.com"
+//     },
+//     {
+//         name : 'person4',
+//         age : 34,
+//         email : "person4@gmail.com"
+//     },
+// ]
+
+// Comparison operators
+
+// =,  ==, ===, !, !=, !==
+//isPerson = false
+
+// Logical operators
+// AND(&&), OR(||), NOT(!)
+
+// let x = 5;
+// let y = 4;
+// let z = true;
+
+// console.log(!z)
+
+// Loops
+// For loop
+// 
+//for ( let i = 0; i <=10; i++) {
+
+    //console.log(i)
+//}
+
+// while
+////let i = 0
+
+//while (i <= 10) {
+    // console.log(i)
+    //i++
+ //}
+//for...of
+//const numbers = [99, 70, 66];
+
+//for (let x of numbers) {
+    //console.log(x)
+//}
+
+//fo..in
+//const numbers = [99, 70, 66];
+//for (let x in numbers) {
+//console.log(x)
+//}
+
+// conditional statement
+ //const x = '10';
+
+// if (x === 10 ) {
+ //   console.log('x is 10');
+ //} else if (x > 10) {
+ //   console.log('x is greater than 10');
+//} else {
+    //console.log('x is less than 10');
+//}
+
+
+//  Switch statement
+// let color = 'black'
+
+// switch(color) {
+//     case 'red' :
+//         console.log('color is red');
+//         break;
+//     case 'blue' : 
+//         console.log('color is blue');
+//         break;
+//     default : 
+//     console.log('color is neither red nor blue');
+// }
+
+// Ternary operator
+// let marks = 50;
+
+// let result = (marks >= 40) ? "Passed" : "Failed"
+
+// console.log(`You ${result} the exams`)
+
+// Function 
+// function sum (x, y) {
+//     return x + y
+// };
+
+// console.log(sum(5, 5))
+
+
+
+
+
